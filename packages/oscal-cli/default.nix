b6182297a7ae8e654442a575663bab2f56554b41
Nikita Wootten <me@nikita.computer>
2024-03-20 18:47:11 -0400

    Update OSCAL-CLI version

--- packages/oscal-cli/default.nix
+++ packages/oscal-cli/default.nix
@@ -12,7 +12,7 @@
 }:
 let
   pname = "oscal-cli";
-  version = "1.0.2";
+  version = "1.0.3";
 in
 pkgs.stdenv.mkDerivation {
   inherit pname version;
@@ -21,7 +21,7 @@ pkgs.stdenv.mkDerivation {
 
   src = pkgs.fetchzip {
     url = "https://repo1.maven.org/maven2/gov/nist/secauto/oscal/tools/oscal-cli/cli-core/${version}/cli-core-${version}-oscal-cli.zip";
-    sha256 = "sha256-BD9OJgvxgF6W2jBp6Wb8RYJgxh21VWP/z9aLW78CY+U=";
+    sha256 = "sha256-s3pyOmYxMaq+EF009F3bq7xXVPQTWTcwZq+4AtNh2cU=";
     stripRoot = false;
   };
 
